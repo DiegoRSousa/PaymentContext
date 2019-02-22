4f28755ec24da1a05c047cced6ddc0faeac2c438
Diego Sousa <diegors90@gmail.com>
2019-02-22 00:20:20 -0300

    first commit

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace PaymentContext.Domain.Entities 
+{
+    public abstract class Payment 
+    {
+        public String Number { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public Decimal Total { get; set; }
+        public Decimal TotalPaid { get; set; }
+        public string Owner { get; set; }
+        public string Document { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }   
+
+    public class BoletoPayment : Payment
+    {
+        public string BarCode { get; set; }
+    }   public string BoletoNumber { get; set; }
+
+    public class CrediCardPayment : Payment
+    {
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string LastTransationNumber { get; set; }
+    }   
+
+    public class PayPalPayment : Payment
+    {
+        public string TransactionCode { get; set; }
+    }   
+}
